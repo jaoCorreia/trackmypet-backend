353b990a35a9194c8190a4586eaa90d9c240084b
João Carlos da Silva Correia <joaocarlos.dasilvacorreia01@gmail.com>
2025-10-29 20:41:18 -0300

    feat: adjust cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,15 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: true,
+    cors: {
+      origin:
+        process.env.NODE_ENV === 'production'
+          ? ['https://yourdomain.com']
+          : true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+      credentials: true,
+    },
   });
   app.setGlobalPrefix('api/v1');
 
