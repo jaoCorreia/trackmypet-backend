6f02eea94d9d3c3109a94b4840b2cf40c9fa5136
João Carlos da Silva Correia <joaocarlos.dasilvacorreia01@gmail.com>
2025-11-29 11:28:47 -0300

    feat: create activity history schedule

--- src/activities/activity.module.ts
+++ src/activities/activity.module.ts
@@ -5,12 +5,23 @@ import { User } from 'src/database/entities/user.entity';
 import { UsersModule } from 'src/users/users.module';
 import { ActivitiesController } from './activity.controller';
 import { ActivitiesService } from './activity.service';
+import { ActivityScheduler } from './activity.scheduler';
 import { Activity } from 'src/database/entities/activity.entity';
+import { ActivitySchedule } from 'src/database/entities/activity-schedule.entity';
+import { ActivityHistory } from 'src/database/entities/activity-history.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Activity, User]), UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      Activity,
+      User,
+      ActivitySchedule,
+      ActivityHistory,
+    ]),
+    UsersModule,
+  ],
   controllers: [ActivitiesController],
-  providers: [ActivitiesService],
+  providers: [ActivitiesService, ActivityScheduler],
   exports: [ActivitiesService],
 })
 export class ActivitiesModule {
